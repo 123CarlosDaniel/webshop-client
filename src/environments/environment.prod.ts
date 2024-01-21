d2e1103eef298ce0683e44b6afa675ba15ac09e8
carlos_daniel <carloschoqque123@gmail.com>
2024-01-21 11:28:57 -0500

    add environment files

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  baseApiUrl: 'http://localhost:8080/api/v1',
+  checkoutUrl: 'http://localhost:8080/api/v1/checkout',
+  authUrl: 'http://localhost:8080/api/v1/auth/login',
+  productsUrl: 'http://localhost:8080/api/v1/products',
+  stripePublicToken: 'pk_test_51OQztuLKVSE3GKcQqonuYeakEp9yjPExXyAB4PEAZmPg00222ImLtLqPbHpXk7uF2rLlVML01hUciESgh67hAhlH009ogyjaG7'
+};
