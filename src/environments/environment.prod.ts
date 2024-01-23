6ab539861eaca2202ece2a54046f70cce2c9bc23
carlos_daniel <carloschoqque123@gmail.com>
2024-01-22 19:42:41 -0500

    update environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  baseApiUrl: process.env['baseApiUrl'],
-  checkoutUrl: process.env['checkoutUrl'],
-  authUrl: process.env['authUrl'],
-  productsUrl: process.env['productsUrl'],
-  stripePublicToken: process.env['stripePublicToken']
+  baseApiUrl: "https://webshop-backend.fly.dev/api/v1",
+  checkoutUrl: "https://webshop-backend.fly.dev/api/v1/checkout",
+  authUrl: "https://webshop-backend.fly.dev/api/v1/login",
+  productsUrl: "https://webshop-backend.fly.dev/api/v1/products",
+  stripePublicToken: "pk_test_51OQztuLKVSE3GKcQqonuYeakEp9yjPExXyAB4PEAZmPg00222ImLtLqPbHpXk7uF2rLlVML01hUciESgh67hAhlH009ogyjaG7"
 };
